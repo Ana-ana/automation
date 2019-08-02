6a6c81266f91460151b162f10b7a3a23471f7ea7
a.peshkova <a.peshkova@tinkoff.ru>
2019-08-02 09:08:54 +0300

    added constructor with comments and getters + setters

--- src/main/java/lesson2/Animal.java
+++ src/main/java/lesson2/Animal.java
@@ -3,17 +3,18 @@ package lesson2;
 public class Animal {
     // type of animal
 
-    public String animalName;
-    public String food;
-    public String game;
-    public int wasBorn;
+    private String animalName; //must be private to help me to restrict access to variable and to add limits
+    private String food;
+    private String game;
+    private int wasBorn;
 
     public String getAnimalName() {
         return animalName;
     }
-    public void setAnimalName(String name) {
+    //added only getter cause wanted to let read only access to this variable
+    /*public void setAnimalName(String name) {
         this.animalName = name;
-    }
+    }*/
     public String getFood() {
         return food;
     }
@@ -23,17 +24,27 @@ public class Animal {
     public String getGame() {
         return game;
     }
-    public void setGame(String food) {
+    public void setGame(String game) {
         this.game = game;
     }
     public int getwasBorn() {
         return wasBorn;
     }
-    public void setWasBorn(int wasBorn) {
+    public void setWasBorn(int wasBorn) { // checking appropriate value
+        if (wasBorn >=0) {
+        this.wasBorn = wasBorn;
+        } else {
+            System.out.println("The year of birth cannot not be negative");
+        }
+    }
+    //constructor with necessary fields for the object
+    //constructor doesn't returning type and has name of the class with big letter
+    public Animal(String animalName, int wasBorn) {
+        this.animalName = animalName;
         this.wasBorn = wasBorn;
     }
-
-
+    //empty constructor for cases when i don't know the name and age
+    public Animal() {}
     public static void eat(String name, String food) {
         System.out.println(food + " - " + " любимая еда " + name);
     }
@@ -44,22 +55,25 @@ public class Animal {
         int curretYear = 2019;
         System.out.println(name + " " + (curretYear - wasBorn) + " лет от роду" );
     }
+    /*public static void voice (Animal, String sound){
+        System.out.println(Animal.);
+    }*/
 
 
 
 
     public static void main(String[] args) {
-       Animal cat = new Animal();
-       cat.animalName = "Барсик";
-       cat.food = "Вискас";
-       cat.game = "веревочкe";
-       eat(cat.animalName, cat.food);
-       play(cat.animalName, cat.game);
+       Animal cat1 = new Animal("Барсик", 1995);
+        eat(cat1.animalName, "Вискас");
+
+       play(cat1.animalName, "tennis");
 
        Animal cat2 = new Animal();
-       cat2.animalName = "Рыжик";
-       cat2.wasBorn = 1994;
+      eat("Васька", "рыбка");
        getAge(cat2.animalName, cat2.wasBorn);
+        cat2.setWasBorn(2015);
+       cat2.setWasBorn(-1975);
+        System.out.println(cat2.wasBorn);
 
 
 
---
+++ src/main/java/lesson2/Cat.java
@@ -0,0 +1,17 @@
+package lesson2;
+
+public class Cat extends Animal {
+    Cat barsik = new Cat();
+/*
+   @Override
+    public void setAnimalName(String name) {
+        super.setAnimalName("Барсик");
+    }
+
+    @Override
+    public void setFood(String food) {
+        super.setFood("Еда для котов");
+    }*/
+
+
+}
